1f8e056b16f37996515f8bc407d6a588e1b50495
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2012-03-08 12:39:18 -0500

    added comments

--- target.js
+++ target.js
@@ -1,3 +1,11 @@
+// We're calling this "asymHumanoidBodyParts" for two reason:
+// * it only contains the left-side body parts
+// * in the future we might want to include creatures like spiders or
+//   wolves or whatever
+//
+// The first element of the nested array is the name of the body part,
+// and the second element is its relative weight - how likely it is
+// that it will be hit relative to the other parts.
 var asymHumanoidBodyParts = [["head", 3],
     ["left-eye", 1],
     ["left-ear", 1],
@@ -18,9 +26,18 @@ var asymHumanoidBodyParts = [["head", 3],
     ["left-achilles", 1],
     ["left-foot", 2]]
 
+// This is used to return a nested array of symmetrical body parts.
+// For example, if the source array of body parts contains
+// ["left-elbow", 2]
+// then the array returned by this function will also include
+// ["right-elbow", 2]
+// This allows us to reduce replication, as a left elbow is just as
+// likely to be hit as a right elbow.
 var symmetrizeBodyParts = function(bodyParts) {
   var finalParts = [];
   
+  // Add the part to the final list of parts.
+  // Also add the right-side version if we're dealing with a left-side part.
   var partAndMatch = function(part) {
     finalParts.push(part)
     if (part[0].indexOf("left") > -1) {
@@ -40,6 +57,19 @@ var symmetrizeBodyParts = function(bodyParts) {
   return addMatchingParts(bodyParts);
 }
 
+
+// The logic of the targeting system is as follows:
+// 1. Sum up the weights of all the body parts
+// 2. Pick a random number between 0 and the sum - the +target+
+// 3. Iterate through the list of body parts, incrementing a +counter+
+//    by the weight of the body part.
+// 4. When the +counter+ is greater than the +target+, then the
+//    corresponding body part has been hit.
+// 5. Return that body part.
+
+// This is used to sum the weights of all the body parts.
+// It's used in conjunction with the target function to return a
+// number which will be used to determine which  body part has been  hit.
 var targetsSum = function(bodyParts) {
   var sum = 0;
   for(i = 0; i < bodyParts.length; i++) {
@@ -48,6 +78,15 @@ var targetsSum = function(bodyParts) {
   return sum;
 }
 
+// Return a number between 0 and the sum of all body part weights.
+var target = function(bodyParts) {
+  return Math.floor(Math.random() * targetsSum(bodyParts));
+}
+
+// This uses the target function to get a number which we can use to 
+// determine which body part has been hit. We must then call the
+// function returned by targetHitFunction in order to actually
+// determine the body part.
 var attack = function(bodyParts) {
   var targetHit = targetHitFunction(target(bodyParts));
   for(i = 0; i < bodyParts.length; i++) {
@@ -57,23 +96,27 @@ var attack = function(bodyParts) {
   }
 }
 
-var target = function(bodyParts) {
-  return Math.floor(Math.random() * targetsSum(bodyParts));
-}
-
+// This function returns a function which is used to test whether a
+// body part corresponds to a given target.
+// Notice that a closure is created which includes both
+//  +target+ and +currentPosition+.
 var targetHitFunction = function(target) {
-  var currentPosition = 0;
+  var weightAccumulator = 0;
   return function(increment) {
-    currentPosition += increment;
-    return currentPosition > target;
+    weightAccumulator += increment;
+    return weightAccumulator > target;
   }
 }
 
+// This is merely to set up the event handlers on the dom objects.
 var setup = function() {
   document.getElementById("initiate-attack").onclick = function() {
-    var partHit = attack(symmetrizeBodyParts(asymHumanoidBodyParts))[0].replace("-", " ")
+    var partHit = attack(symmetrizeBodyParts(asymHumanoidBodyParts))[0].replace(/-/g, " ")
     document.getElementById("attack-message").innerHTML = "You hit the giant's " + partHit + "!";
   }
 }
 
+// Assign the setup function to the onload event handler. Notice that
+// we're not using the parentheses; we're assigning the function
+// object, not the result of a call to the function.
 window.onload = setup;
